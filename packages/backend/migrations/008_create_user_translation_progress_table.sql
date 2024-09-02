cebec7527d9242871ca467a9973338f990db661a
Nikolay Eremeev <nikolay.eremeev@outlook.com>
2024-09-02 15:13:05 +0200

    improving some examples (#31)

--- packages/backend/migrations/008_create_user_translation_progress_table.sql
+++ packages/backend/migrations/008_create_user_translation_progress_table.sql
@@ -1,4 +1,4 @@
-CREATE TABLE user_translation_progress (
+CREATE TABLE IF NOT EXISTS user_translation_progress (
   id SERIAL PRIMARY KEY,
   user_id INTEGER REFERENCES "user" (id) ON DELETE CASCADE,
   word_pair_id INTEGER REFERENCES translation (id) ON DELETE CASCADE,
@@ -8,6 +8,6 @@ CREATE TABLE user_translation_progress (
   UNIQUE (user_id, word_pair_id)
 );
 
-CREATE INDEX idx_user_translation_progress_user ON user_translation_progress (user_id);
+CREATE INDEX IF NOT EXISTS idx_user_translation_progress_user ON user_translation_progress (user_id);
 
-CREATE INDEX idx_user_translation_progress_word_pair ON user_translation_progress (word_pair_id);
+CREATE INDEX IF NOT EXISTS idx_user_translation_progress_word_pair ON user_translation_progress (word_pair_id);
--- packages/backend/migrations/012_create_trigger_update_timestamp.sql
+++ packages/backend/migrations/012_create_trigger_update_timestamp.sql
@@ -4,12 +4,24 @@ BEGIN
    NEW.updated_at = CURRENT_TIMESTAMP;
    RETURN NEW;
 END;
-$$ language 'plpgsql';
+$$ LANGUAGE plpgsql;
 
-CREATE TRIGGER update_user_timestamp BEFORE
-UPDATE ON "user" FOR EACH ROW
-EXECUTE FUNCTION update_timestamp ();
+DO $$
+BEGIN
+  IF NOT EXISTS (SELECT 1 FROM pg_trigger WHERE tgname = 'update_user_timestamp') THEN
+    CREATE TRIGGER update_user_timestamp
+    BEFORE UPDATE ON "user"
+    FOR EACH ROW
+    EXECUTE FUNCTION update_timestamp();
+  END IF;
+END $$;
 
-CREATE TRIGGER update_word_list_timestamp BEFORE
-UPDATE ON word_list FOR EACH ROW
-EXECUTE FUNCTION update_timestamp ();
+DO $$
+BEGIN
+  IF NOT EXISTS (SELECT 1 FROM pg_trigger WHERE tgname = 'update_word_list_timestamp') THEN
+    CREATE TRIGGER update_word_list_timestamp
+    BEFORE UPDATE ON word_list
+    FOR EACH ROW
+    EXECUTE FUNCTION update_timestamp();
+  END IF;
+END $$;
