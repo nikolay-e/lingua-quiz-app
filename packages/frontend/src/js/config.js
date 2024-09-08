b96f04b920a81e0e1b811cd795aef2526b5fecd2
Nikolay Eremeev <nikolay.eremeev@outlook.com>
2024-09-08 17:11:31 +0200

    moving to k3s (#36)

--- packages/frontend/src/js/config.js
+++ packages/frontend/src/js/config.js
@@ -1,6 +1,6 @@
 const serverAddress =
   window.location.hostname === 'lingua-quiz.nikolay-eremeev.com'
-    ? 'https://api-lingua-quiz.nikolay-eremeev.com:443'
-    : 'https://test-api-lingua-quiz.nikolay-eremeev.com:443';
+    ? 'https://api-lingua-quiz.nikolay-eremeev.com'
+    : 'https://test-api-lingua-quiz.nikolay-eremeev.com';
 
 export default serverAddress;
