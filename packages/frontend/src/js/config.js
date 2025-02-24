4b20e73ffdd993e870ae469033ffb28342aaa798
Nikolay Eremeev <nikolay.eremeev@outlook.com>
2025-02-24 22:03:19 +0100

    adding dev docker compose

--- packages/frontend/src/js/config.js
+++ packages/frontend/src/js/config.js
@@ -1,6 +1,26 @@
-const serverAddress =
-  window.location.hostname === 'lingua-quiz.nikolay-eremeev.com'
-    ? 'https://api-lingua-quiz.nikolay-eremeev.com'
-    : 'https://test-api-lingua-quiz.nikolay-eremeev.com';
+// src/js/config.js
+const getServerAddress = () => {
+  const { hostname } = window.location;
+
+  // Check if we're running integration tests
+  if (hostname === undefined) {
+    return 'https://test-api-lingua-quiz.nikolay-eremeev.com';
+  }
+
+  // Check if we're running locally via Docker
+  if (hostname === 'localhost' || hostname === '127.0.0.1') {
+    return 'http://localhost:3000';
+  }
+
+  // Check if we're in test environment
+  if (hostname.includes('test-')) {
+    return 'https://test-api-lingua-quiz.nikolay-eremeev.com';
+  }
+
+  // otherwise production
+  return 'https://api-lingua-quiz.nikolay-eremeev.com';
+};
+
+const serverAddress = getServerAddress();
 
 export default serverAddress;
